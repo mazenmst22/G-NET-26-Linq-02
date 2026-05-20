55c359f3e0f07bc610d79d3ca280bea9357e0b62
Mazen Thabet <MazenThabet2@outlook.com>
2026-05-20 23:55:44 +0300

    Question 1

--- G-NET-26-Linq-02/Program.cs
+++ G-NET-26-Linq-02/Program.cs
@@ -5,7 +5,15 @@ namespace G_NET_26_Linq_02
     {
         public static void Main(string[] args)
         {
-
+            #region Q1
+            var top3 = ProductList
+                .OrderByDescending(p => p.UnitPrice)
+                .Take(3);
+            foreach (var product in top3)
+            {
+                Console.WriteLine($"Name: {product.ProductName}, Price: {product.UnitPrice}");
+            }
+            #endregion
         }
     }
 }
